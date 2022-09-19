4da12a5d71298743d2546e519a886c71b7e94651
samik shk <samik.shakya8848@gmail.com>
2022-09-19 14:00:25 +0545

    crud

--- login/update.php
+++ login/update.php
@@ -1,11 +1,13 @@
 
 <?php 
 include_once 'db_config.php';
-   
+if($_SERVER['REQUEST_METHOD']==="POST")
+{
      $id=$_POST["id1"];
   $task=$_POST["todo1"];
       $query="update todotable set task='$task'  where id='$id'";
   $queryupdate=$con->query($query);
   echo("<script>alert('Updated successfully'); window.location.href='index.php'</script>");
+}
   ?>
     
